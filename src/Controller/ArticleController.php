da4e315fa1a0ff14fb8e30373e7079e770032877
JolanAklin <jolanaklin@gmail.com>
2020-10-07 10:11:36 +0200

    Modified how the article pages are displayed

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,20 +10,20 @@ class ArticleController extends AbstractController
 {
     public function showPage($id)
     {
-        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
-        if ($id != 0 && $id !== null && $id !== false) {
-            $page = $this->getDoctrine()->getRepository(Article::class)->find($id);
-            if ($page !== null) {
-                return $this->render('page/page.html.twig', [
-                    'page' => $page
-                ]);
-            } else {
-                throw $this->createNotFoundException('The page does not exist');
+        try {
+            $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+            if ($id != 0 && $id !== null && $id !== false) {
+                $page = $this->getDoctrine()->getRepository(Article::class)->find($id);
+                if ($page !== null) {
+                    return $this->render('article/page.html.twig', [
+                        'page' => $page
+                    ]);
+                } else {
+                    throw $this->createNotFoundException('The page does not exist');
+                }
             }
-            try {
-            } catch (\Throwable $th) {
-                throw $this->createNotFoundException('The page does not exist');
-            }
+        } catch (\Throwable $th) {
+            throw $this->createNotFoundException('The page does not exist');
         }
     }
 }
