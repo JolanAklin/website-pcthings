0456d40c19f2805c98743f81fb3b2fb4bd926566
JolanAklin <jolanaklin@gmail.com>
2020-10-08 14:20:35 +0200

    Fixed a bug that was preventing the login page to load

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\BlogPost;
+use App\Entity\Article;
+
 
 class SecurityController extends AbstractController
 {
