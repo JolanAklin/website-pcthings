ce38519112c856e0b7ea3975826481f6e82b92e9
JolanAklin <jolanaklin@gmail.com>
2020-10-09 12:11:30 +0200

    Created the base form for the user to change their account

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,25 +8,81 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Entity\BlogPost;
 use App\Entity\Article;
-
+use App\Entity\Image;
+use App\Entity\EditUserType;
+use App\Form\EditUserType as FormEditUserType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+         $this->passwordEncoder = $passwordEncoder;
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        /*if ($this->getUser()) {
+        if ($this->getUser()) {
             return $this->redirectToRoute('index');
-        }*/
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error,'blogs_latest' => $this->getDoctrine()->getRepository(BlogPost::class)->findBlogByDate(),'articles_latest' => $this->getDoctrine()->getRepository(Article::class)->findArticleByDate(),]);
+        return $this->render('security/login.html.twig', 
+        ['last_username' => $lastUsername, 
+        'error' => $error,
+        'blogs_latest' => $this->getDoctrine()->getRepository(BlogPost::class)->findBlogByDate(),
+        'articles_latest' => $this->getDoctrine()->getRepository(Article::class)->findArticleByDate(),
+        ]);
+    }
+
+    public function editUser(Request $request)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $error = null;
+        $user = $this->getUser();
+        $form = $this->get('form.factory')->createNamed('my_name', FormEditUserType::class, $user);
+        try {
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+    
+                //update the password
+                $user = $form->getData();
+                
+                $user->setPassword($this->passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                ));
+    
+                //persist the user entity
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Your account has been successfully updates');
+    
+                return $this->redirectToRoute('edit_user');
+            }
+        } catch (\Throwable $th) {
+            $error = "Something bad happened, try again";
+        }
+
+        return $this->render('security/edit_user.html.twig', 
+        ['error' => $error,
+        'blogs_latest' => $this->getDoctrine()->getRepository(BlogPost::class)->findBlogByDate(),
+        'articles_latest' => $this->getDoctrine()->getRepository(Article::class)->findArticleByDate(),
+        'form' => $form->createView(),
+        ]);
     }
 
     /**
---
+++ src/Form/EditUserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class EditUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class, ['mapped' => false])
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('email', TextType::class)
+            ->add('profilPic', FileType::class, ['mapped' => false])
+            //->add('blogImage')
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
