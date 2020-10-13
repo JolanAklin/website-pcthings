cdb4da434d2f63dca0290b7aa0fcde409ea0a9af
JolanAklin <jolanaklin@gmail.com>
2020-10-13 11:48:59 +0200

    Finished the image importation

--- src/Controller/ImportController.php
+++ src/Controller/ImportController.php
@@ -9,10 +9,12 @@ use App\Entity\BlogPost;
 use App\Entity\Article;
 use App\Entity\Image;
 use App\Form\ImportPictureFormType;
+use App\Form\ModifyPictureFormType;
 use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ImportController extends AbstractController
 {
@@ -21,7 +23,7 @@ class ImportController extends AbstractController
         $this->denyAccessUnlessGranted("ROLE_IMPORT");
 
         $image = new Image();
-        $form = $this->get('form.factory')->createNamed('editUserForm', ImportPictureFormType::class, $image);
+        $form = $this->get('form.factory')->createNamed('ImportPicture', ImportPictureFormType::class, $image);
         $errors = [];
         $form->handleRequest($request);
         try {
@@ -85,5 +87,57 @@ class ImportController extends AbstractController
             'form' => $form->createView(),
             'errors' => $errors
         ]);
+    }
+
+    public function modifyPicture ($imageId, Request $request)
+    {
+        $this->denyAccessUnlessGranted("ROLE_IMPORT");
+        
+        $image = $this->getDoctrine()->getRepository(Image::class)->find($imageId);
+        if($image !== null)
+        {
+            $form = $this->get('form.factory')->createNamed('ImportPicture', ModifyPictureFormType::class, $image);
+            $errors = [];
+            $form->handleRequest($request);
+            try {
+                if ($form->isSubmitted() && $form->isValid()) {
+        
+                    //update the password
+                    $image = $form->getData();
+        
+                    //persist the image entity
+                    $entityManager = $this->getDoctrine()->getManager();
+                    $entityManager->persist($image);
+                    $entityManager->flush();
+        
+                    $this->addFlash('success', 'Your image has been successfully updated');
+        
+                    return $this->redirectToRoute('import_picture');
+                }
+                if($form->isSubmitted())
+                {
+                    //get all the error of the form
+                    foreach ($form as $child) {
+                        foreach ($child->getErrors() as $error) {
+                            array_push($errors, $error->getMessage());
+                        }
+                    }
+                }
+            } catch (\Throwable $th) {
+                array_push($errors, "An error occured at the reading of the form fields");
+            }
+            return $this->render('import/import_picture_modify.html.twig', [
+                'blogs_latest' => $this->getDoctrine()->getRepository(BlogPost::class)->findBlogByDate(),
+                'articles_latest' => $this->getDoctrine()->getRepository(Article::class)->findArticleByDate(),
+                'form' => $form->createView(),
+                'image' => $image,
+                'errors' => $errors
+            ]);
+
+        }else
+        {
+            $this->addFlash('error', 'The requested image doesn\'t exist');
+            return $this->redirectToRoute('import_picture');
+        }
     }
 }
