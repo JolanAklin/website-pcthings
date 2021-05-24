0fc00e21b4a4b99bb7f9e9babbfcccb891ab8e6d
yoyoyzbi <yohan.zbinden@gmail.com>
2021-05-24 17:47:40 +0200

    removed webpack-encore

--- assets/app.jsx
+++
@@ -1,4 +0,0 @@
-import ReactDOM from "react-dom";
-import { TextEditor } from "./components";
-import React from "react";
-ReactDOM.render(<TextEditor />, document.getElementById("root"));
--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
